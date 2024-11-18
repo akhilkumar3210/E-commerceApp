c543d80ecbfe1c937ef9b92a6c16c9ab4cd52fbc
akhilkumar3210 <ak2072719@gmail.com>
2024-11-18 16:09:53 +0530

    user,home

--- ecomm/eapp/urls.py
+++ ecomm/eapp/urls.py
@@ -2,8 +2,19 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.ecom_login),
+     path('logout',views.ecom_logout),
+#----------------Admin----------------------------------------------------------------------
     path('shop_home',views.shop_home),
-    path('logout',views.ecom_logout),
     path('product',views.add_product),
     path('edit/<id>',views.edit_product),
+    path('delete/<pid>',views.delete_product),
+    
+#----------------User----------------------------------------------------------------------
+
+    path('register',views.register),
+    path('user_home',views.user_home),
+    path('logout',views.user_logout),
+    
+
+    
 ]
--- ecomm/eapp/views.py
+++ ecomm/eapp/views.py
@@ -2,18 +2,26 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from .models import *
+import os
+from django.contrib.auth.models import User
 # Create your views here.
 def ecom_login(req):
     if 'shop' in req.session:
         return redirect(shop_home)
+    if 'user' in req.session:
+        return redirect(user_home)
     if req.method=='POST':
         uname=req.POST['uname']
         password=req.POST['password']
         shop=authenticate(username=uname,password=password)
         if shop:
             login(req,shop)
-            req.session['shop']=uname   
-            return redirect(shop_home)
+            if shop.is_superuser:
+                req.session['shop']=uname   
+                return redirect(shop_home)
+            else:
+                req.session['user']=uname   
+                return redirect(user_home)
         else:
             messages.warning(req,'Invaild username or password!!!')
             return redirect(ecom_login)
@@ -69,4 +77,37 @@ def edit_product(req,id):
     else:
         data=Product.objects.get(pk=id)
         return render(req,'shop/edit.html',{'data':data})
+    
+def delete_product(req,pid):
+    data=Product.objects.get(pk=pid)
+    file=data.img.url
+    file=file.split('/')[-1]
+    os.remove('media/'+file)
+    data.delete()
+    return redirect(shop_home)
 #----------------User----------------------------------------------------------------------
+
+def register(req):
+    if req.method=='POST':
+        email=req.POST['email']
+        uname=req.POST['uname']
+        password=req.POST['password']
+        try:
+            data=User.objects.create_user(first_name=uname,email=email,username=email,password=password)
+            data.save()
+            return redirect(ecom_login)
+        except:
+            messages.warning(req,'Email Already Exists!!')
+            return redirect(register)
+    else:
+        return render(req,'user/register.html')
+    
+def user_home(req):
+    if 'user' in req.session:
+        product=Product.objects.all()
+        return render(req,'user/user.html',{'products':product})
+
+def user_logout(req):
+    logout(req)
+    req.session.flush()
+    return redirect(ecom_login)
